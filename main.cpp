90717d3d8312432fff1a790908c4056887670f18
J4yd3n222 <68024246+J4yd3n222@users.noreply.github.com>
2023-12-06 16:51:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,12 @@
 #include <vector>
 using namespace std;
 
-
 string GetInfoString(const string& infoType) {
     string temp;
-    cin >> temp;
+    getline(cin, temp);
     //basic input type/value validation for string, prevents empty strings from being input
     while (temp.length() <= 0) {
-        cout << "Invalid " << infoType << ". Please re-enter a valid option.\n";
+        cout << "Invalid value for " << infoType << ". Please re-enter a valid option.\n";
         cin >> temp;
     }
     return temp;
@@ -32,7 +31,7 @@ int GetInfoNum(const string& infoType, int min = INT_MIN, int max = INT_MAX) {
             }
             break;
         } catch (...) {
-            cout << "Invalid " << infoType << ". Please input a valid value";
+            cout << "Invalid value for " << infoType << ". Please input a valid value";
             if (max != INT_MAX || min != INT_MIN) {
                 cout << " between " << min << " and " << max << "." << endl;
             } else {
@@ -56,7 +55,7 @@ vector<double> CalcBatterStats(string& pname) { //Calculates wOBA, wRAA, & BABIP
     const double wHR = 2.004;
     const double lgwOBA = 0.318;
     const double wOBAScale = 1.204;
-    double uBB, HBP, numSingles, numDoubles, numTriples, homeRuns, AB, IBB, SF, PA, K;
+    int uBB, HBP, numSingles, numDoubles, numTriples, homeRuns, AB, IBB, SF, PA, K;
     string playerName;
     //uBB = number of unintentional walks
     //IBB = intentional walks
@@ -69,98 +68,53 @@ vector<double> CalcBatterStats(string& pname) { //Calculates wOBA, wRAA, & BABIP
 
     cout << "Enter player name:" << endl;
     cin >> ws;
-    getline (cin, playerName);
+    playerName = GetInfoString("player name");
     pname = playerName;
 
     cout << "Enter number of intentional walks:" << endl;
-    IBB = GetInfoNum("intentional walks");
+    IBB = GetInfoNum("intentional walks",0);
 
     cout << "Enter number of total walks:" << endl;
-    cin >> uBB;
-    while (uBB < 0) {
-        cout << "Invalid number of total walks. Please enter a number greater than or equal to zero.." << endl;
-        cin >> uBB;
-    }
+    uBB = GetInfoNum("total walks",0, IBB);
     uBB = uBB - IBB;
 
     cout << "Enter number of hit by pitches:" << endl;
-    cin >> HBP;
-    while (HBP < 0) {
-        cout << "Invalid number of hit by pitches. Please enter a number greater than or equal to zero." << endl;
-        cin >> HBP;
-    }
-
-    cout << "Enter number of singles:" << endl;
-    cin >> numSingles;
-    while (numSingles < 0) {
-        cout << "Invalid number of singles. Please enter a number greater than or equal to zero." << endl;
-        cin >> numSingles;
-    }
-
-    cout << "Enter number of doubles:" << endl;
-    cin >> numDoubles;
-    while (numDoubles < 0) {
-        cout << "Invalid number of doubles. Please enter a number greater than or equal to zero.." << endl;
-        cin >> numDoubles;
-    }
-
-    cout << "Enter number of triples:" << endl;
-    cin >> numTriples;
-    while (numTriples < 0) {
-        cout << "Invalid number of triples. Please enter a number greater than or equal to zero." << endl;
-        cin >> numTriples;
-    }
-
-    cout << "Enter number of home runs:" << endl;
-    cin >> homeRuns;
-    while (homeRuns < 0) {
-        cout << "Invalid number of home runs. Please enter a number greater than or equal to zero." << endl;
-        cin >> homeRuns;
-    }
+    HBP = GetInfoNum("hit by pitches",0);
 
     cout << "Enter number of at bats:" << endl;
-    cin >> AB;
-    while (AB < 0) {
-        cout << "Invalid number of at bats. Please enter a number greater than or equal to zero." << endl;
-        cin >> AB;
-    }
+    AB = GetInfoNum("at bats",0);
+
+    cout << "Enter number of singles:" << endl;
+    numSingles = GetInfoNum("singles",0, AB);
+
+    cout << "Enter number of doubles:" << endl;
+    numDoubles = GetInfoNum("doubles",0, AB-numSingles);
+
+    cout << "Enter number of triples:" << endl;
+    numTriples = GetInfoNum("triples",0, AB-numSingles-numDoubles);
+
+    cout << "Enter number of home runs:" << endl;
+    homeRuns = GetInfoNum("home runs",0, AB-numSingles-numDoubles-numTriples);
 
     cout << "Enter number of plate appearances:" << endl;
-    cin >> PA;
-    while (PA < 0) {
-        cout << "Invalid number of plate appearances. Please enter a number greater than or equal to zero." << endl;
-        cin >> PA;
-    }
+    PA = GetInfoNum("plate appearances",AB);
 
     cout << "Enter number of sacrifice fly outs:" << endl;
-    cin >> SF;
-    while (SF < 0) {
-        cout << "Invalid number of sacrifice fly outs. Please enter a number greater than or equal to zero." << endl;
-        cin >> SF;
-    }
+    SF = GetInfoNum("sacrifice fly outs",0);
 
     cout << "Enter the number of strikeouts:" << endl;
-    cin >> K;
-    while (K < 0) {
-        cout << "Invalid number of strikeouts. Please enter a number greater than or equal to zero." << endl;
-        cin >> K;
-    }
+    K = GetInfoNum("strikeouts",0, AB);
 
     weightedOnBaseAverage = ((uBB * wBB) + (wHBP * HBP) + (wSingles * numSingles) + (wDoubles * numDoubles) +
                              (wTriples * numTriples) + (wHR * homeRuns)) / (AB + uBB + SF + HBP);
     weightedRunsAboveAverage = ((weightedOnBaseAverage - lgwOBA) / wOBAScale) * PA;
-    battingAverageOnBallsInPlay = ((numSingles + numDoubles + numTriples) / (AB - K - homeRuns + SF));
-
-    //cout << fixed << setprecision(3);
-    //cout << playerName << " had a wOBA of " << weightedOnBaseAverage << " in 2023." << endl;
-    //cout << playerName << " had a BABIP of " << battingAverageOnBallsInPlay << " in 2023." << endl;
-    //cout << fixed << setprecision(1);
-    //cout << playerName << " had a wRAA of " << weightedRunsAboveAverage << " in 2023." << endl;
+    battingAverageOnBallsInPlay = ((double) (numSingles + numDoubles + numTriples) / (AB - K - homeRuns + SF));
 
     vector<double> stats{weightedOnBaseAverage, weightedRunsAboveAverage,battingAverageOnBallsInPlay};
     return stats;
 
 }
+
 double CalcPitcherStats(string& pname) { //Calculates FIP
     string pitcherName;
     int homeRuns, walks, hitByPitches, strikeouts;
@@ -171,90 +125,51 @@ double CalcPitcherStats(string& pname) { //Calculates FIP
 
     cout << "Enter pitcher name:" << endl;
     cin >> ws;
-    getline (cin, pitcherName);
+    pitcherName = GetInfoString("pitcher name");
     pname = pitcherName;
 
     cout << "Enter home runs:" << endl;
-    cin >> homeRuns;
-    while (homeRuns < 0) {
-        cout << "Invalid number of home runs.  Please enter a different number:" << endl;
-        cin >> homeRuns;
-    }
+    homeRuns = GetInfoNum("home runs",0);
 
     cout << "Enter the number of walks:" << endl;
-    cin >> walks;
-    while (walks < 0) {
-        cout << "Invalid number of walks.  Please enter a different number:" << endl;
-        cin >> walks;
-    }
+    walks = GetInfoNum("walks",0);
 
     cout << "Enter the number of hit by pitches:" << endl;
-    cin >> hitByPitches;
-    while (hitByPitches < 0) {
-        cout << "Invalid number of hit by pitches.  Please enter a different number:" << endl;
-        cin >> hitByPitches;
-    }
+    hitByPitches = GetInfoNum("hit by pitches",0);
 
     cout << "Enter the number of strikeouts:" << endl;
-    cin >> strikeouts;
-    while (strikeouts < 0) {
-        cout << "Invalid number of strikeouts.  Please enter a different number:" << endl;
-        cin >> strikeouts;
-    }
+    strikeouts = GetInfoNum("strikeouts",0);
 
     cout << "Enter the number of innings pitched:" << endl;
-    cin >> inningsPitched;
-    while (inningsPitched < 0) {
-        cout << "Invalid number of innings pitched.  Please enter a different number:" << endl;
-        cin >> inningsPitched;
-    }
+    inningsPitched = GetInfoNum("innings pitched",0);
 
     FIP = (((13 * double(homeRuns)) + (3 * (walks + hitByPitches)) - (2 * strikeouts)) / inningsPitched) + fipConstant;
-
-    //cout << fixed << setprecision(2);
-    //cout << pitcherName << "'s FIP is " << FIP << endl;
     return FIP;
 }
 
 void CalcTeamStats() { // Calculates & Interprets Pythagorean Winning Percentage
     string teamName;
-    int runsScored, runsAllowed, numGames, losses;
-    double base, exp, winPercentage, expectedWins, winsOverExpected, winsUnderExpected, wins, realWinPercentage;
+    int runsScored, runsAllowed, numGames, losses, wins;
+    double base, exp, winPercentage, expectedWins, winsOverExpected, winsUnderExpected, realWinPercentage;
 
     cout << "Enter team name:" << endl;
     cin >> ws;
-    getline (cin, teamName);
+    teamName = GetInfoString("team name");
 
     cout << "Enter the total runs scored by the " << teamName << endl;
-    cin >> runsScored;
-    while (runsScored < 0) {
-        cout << "Invalid number of runs scored.  Please enter a different number:" << endl;
-        cin >> runsScored;
-    }
+    runsScored = GetInfoNum("runs scored",0);
 
     cout << "Enter the total runs allowed by the " << teamName << endl;
-    cin >> runsAllowed;
-    while (runsAllowed < 0) {
-        cout << "Invalid number of runs allowed.  Please enter a different number:" << endl;
-        cin >> runsAllowed;
-    }
+    runsAllowed = GetInfoNum("runs scored",0);
 
     cout << "Enter the total number of wins by the " << teamName << endl;
-    cin >> wins;
-    while (wins < 0) {
-        cout << "Invalid number of wins.  Please enter a different number:" << endl;
-        cin >> wins;
-    }
+    wins = GetInfoNum("wins",0);
 
     cout << "Enter the total of losses by the " << teamName << endl;
-    cin >> losses;
-    while (losses < 0) {
-        cout << "Invalid number of losses.  Please enter a different number:" << endl;
-        cin >> losses;
-    }
+    losses = GetInfoNum("losses", 0);
 
     numGames = wins + losses;
-    realWinPercentage = wins / numGames;
+    realWinPercentage = (double) wins / numGames;
     base = ((double)runsScored + runsAllowed) / numGames;
     exp = pow(base, 0.287);
     winPercentage = (pow(runsScored, exp)) / (pow(runsScored, exp) + pow(runsAllowed, exp));
@@ -277,23 +192,19 @@ void CalcTeamStats() { // Calculates & Interprets Pythagorean Winning Percentage
     }
 }
 
-
 int main() {
     cout << "Welcome to the advanced baseball stats calculator." << endl
          << "Would you like to know about a batter, pitcher, or a team?" << endl;
     cout << "For batter enter 1" << endl << "For pitcher enter 2" << endl << "For team enter 3" << endl;
     int statsSelection;
-    cin >> statsSelection;
-    while (statsSelection < 1 || statsSelection > 3) {
-        cout << "Invalid selection.  Please enter 1 for batter, 2 for pitcher, or 3 for team." << endl;
-        cin >> statsSelection;
-    }
+    statsSelection = GetInfoNum("value", 1, 3);
 
     if (statsSelection == 1) {
         string p1name;
         vector<double> batter1 = CalcBatterStats(p1name);
         cout << "Would you like to compare batter stats with another batter? Enter 1 for yes and 2 for no.";
         int option = GetInfoNum("option", 1, 2);
+
         if (option == 1) {
             string p2name;
             vector<double> batter2 = CalcBatterStats(p2name);
@@ -302,7 +213,6 @@ int main() {
             cout << p1name << " had a BABIP of " << batter1[2] << " while " << p2name << " had a BABIP of " << batter2[2] << "." << endl;
             cout << fixed << setprecision(1);
             cout << p1name << " had a wRAA of " << batter1[1] << " while " << p2name << " had a wRAA of " << batter2[1] << "." << endl;
-
         } else if (option == 2) {
             cout << fixed << setprecision(3);
             cout << p1name << " had a wOBA of " << batter1[0] << " in 2023." << endl;
@@ -310,12 +220,12 @@ int main() {
             cout << fixed << setprecision(1);
             cout << p1name << " had a wRAA of " << batter1[1] << " in 2023." << endl;
         }
-
     } else if (statsSelection == 2) {
         string p1name;
         double pitcher1 = CalcPitcherStats(p1name);
         cout << "Would you like to compare pitcher stats with another pitcher? Enter 1 for yes and 2 for no.";
         int option = GetInfoNum("option", 1, 2);
+
         if (option == 1) {
             string p2name;
             double pitcher2 = CalcPitcherStats(p2name);
@@ -328,6 +238,4 @@ int main() {
     } else if (statsSelection == 3) {
         CalcTeamStats();
     }
-
-
 }
